b68da47510281694c9147e810d27467341f08adb
Jeff Su <jeffdsu@gmail.com>
2016-05-05 14:44:49 -0700

    add user page controller

--- Frontend/controllers/userDetailsController.js
+++ Frontend/controllers/userDetailsController.js
@@ -1,6 +1,19 @@
-blankApp.controller('userDetailsController', ['$scope', '$resource', '$routeParams', '$http', 'bookService', function ($scope, $resource, $routeParams, $http, bookService) {
+blankApp.controller('userDetailsController', ['$scope', '$resource', '$routeParams', '$http', 'userService', function ($scope, $resource, $routeParams, $http, userService) {
 
+    user_id = $routeParams.userId;
     
+    userService.get(user_id)
+    .then(function(data){
+        $scope.user = data;
+    }, function(err){
+        
+    });
+    
+    userService.get_insights(user_id)
+    .then(function (data) {
+                    $scope.insights = data;
+                }, function (err) {
 
+                })
 
 }]);
--- Frontend/services/user.js
+++ Frontend/services/user.js
@@ -1,31 +1,30 @@
-blankApp.service('authorService', ['$http', '$localStorage', 'urls', '$q', function ($http, $localStorage, urls, $q) {
+blankApp.service('userService', ['$http', '$localStorage', 'urls', '$q', function ($http, $localStorage, urls, $q) {
     self = this;
 
-
     self.get = function (id) {
-        return $http.get(urls.BASE + "/inspiration-corner/authors/" + id)
+        return $http.get(urls.BASE + "/inspiration-corner/users/" + id)
             .then(function (response) {
                 if (typeof response.data === 'object') {
-                    return response.data
+                    return response.data;
                 } else {
                     return $q.reject(response.data)
                 }
             }, function (response) {
                 return $q.reject(response.data)
-            })
-    };
+            });
+    }
     
-    self.get_books = function (id) {
-        return $http.get(urls.BASE + "/inspiration-corner/authors/" + id + "/books")
+    self.get_insights = function (id) {
+        return $http.get(urls.BASE + "/inspiration-corner/users/" + id + "/insights")
             .then(function (response) {
                 if (typeof response.data === 'object') {
-                    return response.data
+                    return response.data;
                 } else {
                     return $q.reject(response.data)
                 }
             }, function (response) {
                 return $q.reject(response.data)
-            })
+            });
     };
 
 }]);
