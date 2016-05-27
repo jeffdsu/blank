246261099884f108ad7ab28e82a875a86cf633dc
Jeff Su <jeffdsu@gmail.com>
2016-05-27 01:33:03 -0700

    ;ij;oij

--- inspiration/models/MediumType.py
+++ inspiration/models/MediumType.py
@@ -2,7 +2,10 @@ from .InspirationBaseModelMixin import InspirationBaseModelMixIn
 from django.db import models
 
 class MediumType(models.Model, InspirationBaseModelMixIn):
+    # TODO need to migrate to plural
     name = models.CharField(max_length=255)
+    # TODO - Jeff this should not be true.
+    singular_eng_noun = models.CharField(null=True,max_length=255)
     icon_url = models.CharField(max_length=255)
 
     def __str__(self):
