7b47cbede08b8d36f8bbcc7813486c59cbe035b1
Jeff Su <jeffdsu@gmail.com>
2016-06-06 12:45:54 -0700

    asdfasdf

--- Frontend/constants.js
+++ Frontend/constants.js
@@ -1,5 +1,5 @@
 // CONSTANTS
 blankApp.constant('urls', {
-       BASE: 'http://127.0.0.1:8000',
+       BASE: 'http://ec2-52-37-29-169.us-west-2.compute.amazonaws.com',
        BASE_API: 'http://api.jwt.dev:8000/v1'
    })
