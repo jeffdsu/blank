56d1ecb7e06810a1ed854a0bef6d24eb9cad3585
Jeff Su <jeffdsu@gmail.com>
2016-06-13 15:43:45 -0700

    make blank community

--- Frontend/constants.js
+++ Frontend/constants.js
@@ -1,5 +1,5 @@
 // CONSTANTS
 blankApp.constant('urls', {
-       BASE: 'http://ec2-52-37-29-169.us-west-2.compute.amazonaws.com',
+       BASE: 'http://blank.community',
        BASE_API: 'http://api.jwt.dev:8000/v1'
    })
