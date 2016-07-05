e006061eddc2c8b59b21f6751e40129fcd977be2
Jeff Su <jeffdsu@gmail.com>
2016-07-04 21:13:16 -0700

    add submission time

--- inspiration/models/Moment.py
+++ inspiration/models/Moment.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .InspirationBaseModelMixin import InspirationBaseModelMixIn
+from datetime import datetime
 
 class Moment(models.Model, InspirationBaseModelMixIn):
     title = models.CharField(max_length=255)
@@ -9,6 +10,7 @@ class Moment(models.Model, InspirationBaseModelMixIn):
     link = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.ForeignKey('MomentType', on_delete=models.CASCADE, null=True)
+    submittion_time = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return "<Moment>"
